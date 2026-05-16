d1ce0c9166d9be00443a52c003fa846fdf651d31
Mattias Andrée <m@maandree.se>
2026-05-16 20:29:49 +0200

    Add sanitizer options to config.mk
    Signed-off-by: Mattias Andrée <m@maandree.se>

--- config.mk
+++ config.mk
@@ -3,6 +3,12 @@ MANPREFIX = $(PREFIX)/share/man
 
 CC = cc
 
+COMMON_SANITIZE = -fsanitize=alignment,shift,signed-integer-overflow,object-size,null,undefined,bounds,address
+CLANG_SANITIZE  = -O1 $(COMMON_SANITIZE),cfi -flto -fvisibility=hidden -fno-sanitize-trap=cfi
+GCC_SANITIZE    = -O1 $(COMMON_SANITIZE)
+#SANITIZE        = $(CLANG_SANITIZE)
+#SANITIZE        = $(GCC_SANITIZE)
+
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_GNU_SOURCE
-CFLAGS   = -std=c11 -Wall -O3
-LDFLAGS  = -lblake -s
+CFLAGS   = $(SANITIZE) -std=c11 -Wall -O3
+LDFLAGS  = $(SANITIZE) -lblake -s
